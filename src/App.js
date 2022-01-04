321860ae722901aaa7181cb110c5c8f4db10749f
Mahmoud Missaoui <mahmoud.missaoui@smtech.consulting>
2022-01-04 11:32:11 +0100

    Commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import "./App.css";
 const arrayFeature = [
   {
     title: "Mission",
-    desc: "Our main mission is to support out customers in the development of their IT systems",
+    desc: "Our main mission is to support our customers in the development of their IT systems",
   },
   {
     title: "Vision",
@@ -17,7 +17,7 @@ const arrayFeature = [
   },
   {
     title: "Values",
-    desc: `We keep our promises and are committed to doing buisiness the right way,      
+    desc: `We keep our promises and are committed to doing business the right way,      
   We provide quality products and services tham meet our customers needs
   and We encourage innovation to meet challenges`,
   },
@@ -41,15 +41,19 @@ const App = () => {
       case "about":
         aboutUsRef.current.scrollIntoView({ behavior: "smooth" });
         break;
+
       case "offers":
         offersRef.current.scrollIntoView({ behavior: "smooth" });
         break;
+
       case "services":
         servicesRef.current.scrollIntoView({ behavior: "smooth" });
         break;
+
       case "contact":
         contactUsRef.current.scrollIntoView({ behavior: "smooth" });
         break;
+
       default:
         console.log("No view to scroll in!!");
     }
@@ -74,6 +78,7 @@ const App = () => {
             <li>
               <img src={Logo} alt="logo" />
             </li>
+
             <ul>
               <li onClick={() => clickMenuHandler("about")}>About us</li>
               <li onClick={() => clickMenuHandler("offers")}>Offers</li>
@@ -82,6 +87,7 @@ const App = () => {
             </ul>
           </ul>
         </nav>
+
         <div
           key={descIndex}
           className="w-25"
@@ -90,6 +96,7 @@ const App = () => {
           <h1 className="animate__animated animate__fadeInUp">
             {arrayFeature[descIndex].title}
           </h1>
+
           <h4 className="animate__animated animate__fadeInUp">
             {arrayFeature[descIndex].desc}{" "}
           </h4>
@@ -100,19 +107,26 @@ const App = () => {
           <div className="col-12">
             <h1>About us</h1>
           </div>
+
           <div className="col-md-6 background-black">
             <h2>
-              Our main mission is to support out customers in the development of
+              Our main mission is to support Our customers in the development of
               their IT systems.
+              <br />
               <br />
               Our vision is to create a better relationship between customers
               and IT services providers.
               <br />
-              We keep our promises and are committed to doing buisiness the
-              right way, We provide quality products and services tham meet our
-              customers needs and We encourage innovation to meet challenges.
+              <br />
+              We keep our promises and are committed to doing business the right
+              way.
+              <br />
+              <br />
+              We provide quality products and services that meet our customers
+              needs and we encourage innovation to meet challenges.
             </h2>
           </div>
+
           <div className="col-md-6">
             <img src={About} alt="about" />
           </div>
@@ -121,15 +135,20 @@ const App = () => {
           <div className="col-12">
             <h1>Offers</h1>
           </div>
+
           <div className="col-md-6">
             <img src={Offers} alt="offers" />
           </div>
+
           <div className="col-md-6 background-black">
             <h2>
-              SMTech has a good expertise in the domaine of web development.
+              SM Tech Consulting has a good expertise in the domain of web
+              development.
+              <br />
               <br />
               We help you to transform your ideas and your needs into full web
               applications.
+              <br />
               <br />
               Also we manage for you all of your on production softwares.
             </h2>
@@ -139,19 +158,23 @@ const App = () => {
           <div className="col-12">
             <h1>Services</h1>
           </div>
+
           <div className="col-md-6 background-black">
             <h2>
               Our expertise allows to offer the best services related to web
               development.
               <br />
-              We can help you preparing fonctional needs as well as to develop a
-              full application.
               <br />
-              Our exprimented enfineers will be delighted to provide you the
+              We can help you preparing fonctional requirements as well as to
+              develop a full application.
+              <br />
+              <br />
+              Our exprimented engineers will be delighted to provide you the
               best services and guide you to make your daily IT activities more
               comfortable.
             </h2>
           </div>
+
           <div className="col-md-6">
             <img src={Services} alt="services" />
           </div>
@@ -160,26 +183,30 @@ const App = () => {
           <div className="col-12">
             <h1>Contact us</h1>
           </div>
+
           <div className="col-md-6">
             <img src={Contact} alt="contact" />
           </div>
+
           <div className="col-md-6 background-black">
             <div className="contact-form">
               <input
                 onChange={changeHandler("name")}
                 type="text"
-                placeholder="Full Name"
+                placeholder=" Your Full Name"
+              />
+              <input
+                onChange={changeHandler("email")}
+                type="email"
+                placeholder=" Your Email"
               />
               <input
                 onChange={changeHandler("subject")}
                 type="text"
                 placeholder="Object"
               />
-              <input
-                onChange={changeHandler("body")}
-                type="text"
-                placeholder="Message"
-              />
+
+              <textarea placeholder="Message..." />
               <button
                 clickHandler={() =>
                   (window.location = `mailto:mahmoud.missawi@gmail.com?subject=${encodeURIComponent(
@@ -194,7 +221,7 @@ const App = () => {
         </section>
       </main>
       <footer className="d-flex justify-content-center">
-        <p>All right reserved Smtech® {new Date().getFullYear()}</p>
+        <p>All right reserved SM Tech Consulting® {new Date().getFullYear()}</p>
       </footer>
     </div>
   );
